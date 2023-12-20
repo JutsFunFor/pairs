a5fae82478342a65d7f4ceee6ac4ee59e3015231
JustFunFor <just_fun_for_fun@inbox.ru>
2023-12-20 20:28:19 +0300

    upd

--- pairs.cpp
+++ pairs.cpp
@@ -7,22 +7,48 @@
 
 class DataProcessor {
 public:
-    using Data = std::vector<std::pair<long, float>>;
-
     DataProcessor() = default;
     ~DataProcessor() = default;
 
+    // creation of processDir function that handle all processing steps
+    void processDir(const char* dir_path, const char* target_curr, long long volume) {
+        
+        // search for csv files in directory
+        std::vector<char*> csvFiles = parseCsv(dir_path);
+        // read data from csv files 
+        std::vector<Data> allData = readData(csvFiles, target_curr);
+        // multiply data taking into account volume parameter 
+        Data data_k = multiplyData(volume, allData);
+        // save data inside ./out.csv file
+        writeData("./out.csv", data_k);
+
+        // clear space 
+        for (char* fileName : csvFiles) {
+            delete[] fileName;
+        }
+    }
+private:
+    // structure for storing data points long for timestamp and double for price values
+    using Data = std::vector<std::pair<long, double>>;
+    
     std::vector<Data> readData(std::vector<char*>& paths, const char* target) {
+        /*
+        readData:
+            input  |- paths  :  vector containing paths to csv files in specified previously folder
+                   |- target :  currency pair wich price should be reversed (ex. EUR/CHF => CHF/EUR)
+            ----------------------------------------------------------------------------------------
+            output |- allData:  vector<Data> that contains data from each csv file
+        */  
         std::vector<Data> allData;
 
         for (const char* path : paths) {
             Data data;
             long t;
-            float p;
+            long double p;
             bool reverse = compareTargetCurr(path, target);
             if (FILE* fp = fopen(path, "r")) {
-                while (fscanf(fp, "%ld;%f", &t, &p) == 2) {
-                    data.push_back(std::make_pair(t, (reverse ? 1.0f / p : p)));
+                while (fscanf(fp, "%ld;%llf", &t, &p) == 2) {
+                    data.push_back(std::make_pair(t, (reverse ? 1 / p : p)));
                 }
                 fclose(fp);
             } else {
@@ -34,6 +60,9 @@ public:
     }
 
     bool fileExists(const char* filename) {
+        /*
+        helper function to check weither file with filename exisits.
+        */
         FILE* file;
         if (file = fopen(filename, "r")) {
             fclose(file);
@@ -43,6 +72,11 @@ public:
     }
 
     void writeData(const char* out_path, const Data& data) {
+        /*
+        writeData:
+            input |- out_path :  name of saved file (./out.csv)
+                  |- data     :  data that should be saved
+        */
         if (fileExists(out_path)) {
             std::cout << "File 'out.csv' already exists. Do you want to overwrite? (Y/N): ";
             char response;
@@ -56,7 +90,7 @@ public:
 
         if (FILE* fp = fopen(out_path, "w+")) {
             for (const auto& pair : data) {
-                fprintf(fp, "%ld;%f\n", pair.first, pair.second);
+                fprintf(fp, "%ld;%llf\n", pair.first, pair.second);
             }
             fclose(fp);
             std::cout << "Data written to 'out.csv' successfully." << std::endl;
@@ -64,7 +98,14 @@ public:
             perror("Error opening file");
         }
     }
-    Data multiplyData(int k, const std::vector<Data>& dataVec) {
+    Data multiplyData(long long volume, const std::vector<Data>& dataVec) {
+        /*
+        multiplyData:
+            input   |- volume  :  volume to be multiplied on prices values
+                    |- dataVec :  currency pair wich price should be reversed (ex. EUR/CHF => CHF/EUR)
+            --------------------------------------------------------------------------------------------------
+            output  |- data_k  :  Data data_k that contains prices multiplication between all files and volume
+        */
         if (dataVec.empty()) {
             std::cerr << "No data\n";
             return Data();
@@ -81,31 +122,41 @@ public:
         Data data_k;
         data_k.reserve(dataSize);
         for (size_t i = 0; i < dataSize; i++) {
-            float p = 1.0f;
+            long double p = 1.0;
             for (const Data& data : dataVec) {
-                p *= k * data[i].second;
+                p *= data[i].second;
             }
-            data_k.push_back(std::make_pair(dataVec[0][i].first, p));
+            data_k.push_back(std::make_pair(dataVec[0][i].first, volume*p));
         }
 
         return data_k;
     }
 
     std::vector<char*> parseCsv(const char* path) {
+
+        /*
+        parseCsv:
+            input   |- path      :  path to directory with csv files
+            --------------------------------------------------------
+            output  |- csvFiles  :  vector with csv filenames
+        */
+
         std::vector<char*> csvFiles;
         namespace fs = std::filesystem;
         try {
             
             for (const auto& entry : fs::directory_iterator(path)) {
                 if (entry.is_regular_file() && entry.path().extension() == ".csv") {
+                    if (entry.path().filename() == "out.csv")
+                        continue;
                     char* fileName = new char[entry.path().filename().string().size() + 1];
                     std::strcpy(fileName, entry.path().filename().string().c_str());
                     csvFiles.push_back(fileName);
                 }
             }
         } catch (const fs::filesystem_error& e) {
-            std::cerr << "Error accessing directory: " << e.what() << std::endl;
-            // Clear allocated memory if an error occurs
+            std::cerr << "Error accessing directory: " << e.what() << "\n";
+        
             for (char* fileName : csvFiles) {
                 delete[] fileName;
             }
@@ -115,51 +166,49 @@ public:
         return csvFiles;
     }
 
-    char* concatenateStrings(const char* str1, const char* str2) {
-        size_t len1 = strlen(str1);
-        size_t len2 = strlen(str2);
-
-        char* concatenated = new char[len1 + len2 + 1];
-
-        strcpy(concatenated, str1);
-        strcat(concatenated, str2);
-        return concatenated;
-    }
-
     bool compareTargetCurr(const char* path, const char* target) {
+
+        /*
+        parseCsv:
+            input   |- path      :  path to directory with csv files
+                    |- target    :  target currency pairs to be founded in raw .csv filename for further reversing
+            ------------------------------------------------------------------------------------------------------
+            output  |- bool      :  true if raw .csv filename contains target currency pair, false - otherwise
+        */
+
         const char* ext_s = strrchr(path, '.');
         if (ext_s == nullptr) {
-            return false; // No extension found
+            return false;
         }
 
         size_t fname_l = ext_s - path;
         size_t target_l = strlen(target);
 
         if (target_l != fname_l) {
-            return false; // Length mismatch
+            return false;
     }
 
     return strncmp(path, target, target_l) == 0;
 }
-    void processDir(const char* dir_path, const char* target_curr, int volume) {
-        std::vector<char*> csvFiles = parseCsv(dir_path);
-        std::vector<Data> allData = readData(csvFiles, target_curr);
-        Data data_k = multiplyData(volume, allData);
-        writeData("./out.csv", data_k);
 
-        // Clear allocated memory for filenames
-        for (char* fileName : csvFiles) {
-            delete[] fileName;
-        }
-    }
 };
 
 int main(int argc, char* argv[]) {
-    const char* dir_path = "./";
-    const char* target_curr = "EURCHF";
-    int volume = 1;
 
+    // specify usage
+    if (argc < 4){
+        std::cerr<<"\nUsage: \n"<<argv[0]<< " <directory_path> <target_currency_pair> <volume>\n\n";
+        return 1;
+    }
+
+    // get params from command line
+    const char* dir_path = argv[1];
+    const char* target_curr = argv[2];
+    long long volume = std::atoll(argv[3]);
+
+    // init
     DataProcessor proc;
+    // process
     proc.processDir(dir_path, target_curr, volume);
 
     return 0;
